7895298a43d23f91955377d06db66f20efbc587d
Alexander Bercow <alexanderbercow@AlexandMBPBlack.home>
2020-02-01 17:28:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function calculation({name, price}, e) {
+  e.preventDefault();
+  var buy = (({price} * 0.95) -300);
+  console.log(`${name}` + buy);
+}
+
+class Submit extends React.Component{
+  render (){
+   return( 
+    <form onSubmit={calculation}>
+        <label>
+            Name: 
+            <input type="text" name=""/>
+        </label>
+        <label>
+          Price:
+            <input type="text" price=""/>
+        </label>
+        <input type="submit" value="Submit"/>
+    </form>
+    )
+  }
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <Submit/>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
